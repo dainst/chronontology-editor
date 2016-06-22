3ed1022d1de4bcf6674a3181e2e40fdb12372463
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-06-22 12:12:24 +0200

    Implement find so that it works at least for resource id as query string.

--- app/rest-datastore.ts
+++ app/rest-datastore.ts
@@ -12,9 +12,6 @@ export class RestDatastore implements Datastore {
 
     constructor(private http:Http) { }
 
-    private db: Promise<any>;
-    private docCache: { [id: string]: Document } = {};
-
     public getUnsyncedObjects(): Observable<Document> {
         return undefined;
     }
@@ -39,7 +36,6 @@ export class RestDatastore implements Datastore {
             this.http.put("/data"+resourceId,JSON.stringify(doc))
                 .subscribe(
                     data => {
-                        this.docCache[resourceId]=doc;
                         resolve()
                     },
                     err => {
@@ -55,44 +51,36 @@ export class RestDatastore implements Datastore {
     }
 
     public get(resourceId:string):Promise<Document> {
-
-        if (this.docCache[resourceId]) {
-            return new Promise((resolve, reject) => resolve(this.docCache[resourceId]));
-        } else {
-            return this.fetchObjectViaHttp(resourceId);
-        }
+        return this.fetchObjectViaHttp(resourceId);
     }
 
     public remove(id:string):Promise<any> {
         return new Promise((resolve, reject) => {
-            this.db.then(db => {
-               resolve();
-            });
+           resolve();
         });
     }
 
     public clear():Promise<any> {
         return new Promise((resolve, reject) => {
-            this.db.then(db => {
-                resolve();
-            });
+            resolve();
         });
     }
 
 
-    // TODO implement find via http
     public find(query:string):Promise<Document[]> {
 
-        query = query.toLowerCase();
-
-        var results : Document[] = [];
-        for (var i in this.docCache) {
-            if (this.docCache[i]['resource']['@id'].indexOf(query)!=-1) results.push(this.docCache[i]);
-        }
-        console.log("results ",results)
-
         return new Promise((resolve, reject) => {
-            resolve(results);
+
+            var url= "/data";
+            this.http.get(url+query)
+                .subscribe(
+                    data => {
+                        const document=JSON.parse(data['_body'])
+                        document['resource']['@id']=document['@id']; // TODO necessary as long as resource id is not fully supported by jeremy
+                        resolve([document])
+                    },
+                    err => reject(err)
+                );
         });
     }
 
@@ -114,7 +102,7 @@ export class RestDatastore implements Datastore {
                     data => {
                         var document=JSON.parse(data['_body']);
                         document['resource'].type="Period";
-                        document['resource']['@id']=document['@id'];
+                        document['resource']['@id']=document['@id']; // TODO necessary as long as resource id is not fully supported by jeremy
                         resolve(document)
                     },
                     err => console.error(err)
